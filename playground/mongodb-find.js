d91a5f88ae07cf67ea2f1fe78fe2617e33eb50f6
dinohasanic@gmail.com <dinohasanic@gmail.com>
2016-11-02 12:37:02 -0700

    Init commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,34 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  // db.collection('Todos').find({
+  //   _id: new ObjectID('581a396560f7603133bd9a16')
+  // }).toArray().then((docs) => {
+  //   console.log('Todos');
+  //   console.log(JSON.stringify(docs, undefined, 2));
+  // }, (err) => {
+  //   console.log('Unable to fetch todos', err);
+  // });
+
+  db.collection('Todos').find().count().then((count) => {
+    console.log(`Todos count: ${count}`);
+  }, (err) => {
+    console.log('Unable to fetch todos', err);
+  });
+
+  db.collection('Users').find({
+    name: "Dino"
+  }).toArray().then((docs) => {
+    console.log(docs);
+  }, (err) => {
+    console.log('Unable to get users', err);
+  });
+
+  // db.close();
+});
